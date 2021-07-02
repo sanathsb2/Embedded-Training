2292f54fc1322682325061bfbcb228238b86bfe1
sanathsb2 <86304873+sanathsb2@users.noreply.github.com>
2021-07-02 20:29:07 +0530

    Update clear_set_toggle_macro.c

--- 26th_june/clear_set_toggle_macro.c
+++ 26th_june/clear_set_toggle_macro.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define CLEAR(number,bit)     number &= ~(1 << bit)
+#define SET(number,bit)       number |= (1<<bit)
+#define TOGGLE(number,bit)    number ^= (1<<bit)
+
+int main(void)
+{   int number = 7;
+    int temp1,temp2,temp3;
+    temp2=temp2=temp3=number;
+    int bit = 3;
+    CLEAR(temp1,bit);
+    printf("%d\n",temp1);
+    SET(temp2,bit);
+    printf("%d\n",temp2);
+    TOGGLE(temp3,bit);
+    printf("%d",temp3);
+    return 0;
+}
